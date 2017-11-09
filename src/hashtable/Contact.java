1f821e38a3563b8dce7b6ef38f811bc7044a6d58
Rowayda-Khayri <rowayda_khayri@yahoo.com>
2017-11-09 14:19:32 +0200

    create Contact class

---
+++ src/hashtable/Contact.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashtable;
+
+/**
+ * new Struct to contain the contact name and phone
+ * @author row
+ */
+public class Contact {
+    
+    
+    String name = new String();
+    int phone ;
+    int linearProb = 0; //
+}
